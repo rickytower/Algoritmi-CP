d23afcbf5b5d4ad7b33c09e7fe9c72d65fc992bd
Riccardo Torre <lalincedeldeserto@gmail.com>
2025-07-14 14:57:07 +0200

    scrivere in dp. La soluzione ricorsiva funziona ma non ha memoizzazione, quella iterativa non funziona

---
+++ paths.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define lui long unsigned int
+using namespace std;
+
+const int grid[5][5] = {
+	{3, 7, 9, 2, 7},
+	{9, 8, 3, 5, 5},
+	{1, 7, 9, 8, 5},
+	{3, 8, 6, 4, 10},
+	{6, 3, 9, 7, 8}
+};
+
+void printsum(int sum[5][5])
+{
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			cout << sum[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl << endl;
+}
+
+int sum(int y, int x)
+{
+	if (x < 0 || y < 0) {
+		return 0;
+	}
+
+	return max(sum(y, x - 1), sum(y - 1, x)) + grid[y][x];
+}
+
+int solve()
+{
+	int sum[5][5];
+	for (int y = 0; y < 5; y++) {
+		for (int x = 0; x < 5; x++) {
+			if (x == 0 || y == 0) {
+				sum[y][x] = grid[y][x];
+			} else {
+				sum[y][x] =
+				    max(sum[y][x - 1],
+					sum[y - 1][x]) + grid[y][x];
+			}
+		}
+
+		printsum(sum);
+	}
+	return sum[4][4];
+}
+
+int main()
+{
+	cout << sum(4, 4) << endl;
+	cout << solve() << endl;
+	return 0;
+}
