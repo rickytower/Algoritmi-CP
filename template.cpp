557af6eec828d7241c2c4944bbc973502781c937
Riccardo Torre <lalincedeldeserto@gmail.com>
2025-07-17 15:29:42 +0200

    rtal

--- template.cpp
+++ template.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define lui long unsigned int
+#define INPUT "input-s.txt"
+#define OUTPUT "output-s.txt"
 using namespace std;
+
+/*soluzione*/
 
 int main()
 {
+	freopen(INPUT, "r", stdin);
+	freopen(OUTPUT, "w", stdout);
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		/* caso test t */
+	}
 	return 0;
 }
